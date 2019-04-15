4baf91dd192370cc837f112c1ba2203e3ed5a4cf
Greg Morgan <gregory.morgan@flatironschool.com>
2019-04-15 16:53:24 -0700

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array[array.length-1]
+  array.last
 end
